bb39e6226208ea5427180429668853e05ad59197
Kyle Gribble <dribbleGribble0516@gmail.com>
2020-10-19 22:44:00 -0500

    Pushing to heroku

---
+++ client/build/precache-manifest.c7904c1aaf5ec4b9f748d70ebb4e62c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d1bbfae2e5b2b76015601dfe1a9c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba52a10db9ea49a7ba17",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c8f8b7b58f9c0e91f088",
+    "url": "/static/css/main.dfde1e7f.chunk.css"
+  },
+  {
+    "revision": "ba52a10db9ea49a7ba17",
+    "url": "/static/js/2.29cf236c.chunk.js"
+  },
+  {
+    "revision": "0adf7f1b9434a120a5bce5b5824fa305",
+    "url": "/static/js/2.29cf236c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f8b7b58f9c0e91f088",
+    "url": "/static/js/main.e8ce7030.chunk.js"
+  },
+  {
+    "revision": "7d015e9475c5eb4c3bbb",
+    "url": "/static/js/runtime-main.9cd1f6d3.js"
+  }
+]);
